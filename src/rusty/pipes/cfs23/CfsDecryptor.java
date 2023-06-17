e70be46989cea8a22ee3371413f9c2885c0e47fc
imacomputer1990 <imacomputer1990@gmail.com>
2023-06-16 20:02:47 -0400

    init

---
+++ src/rusty/pipes/cfs23/CfsDecryptor.java
@@ -0,0 +1,29 @@
+package rusty.pipes.cfs23;
+
+public class CfsDecryptor 
+{
+    private static final String XOR_KEY = "4282840";
+    
+    public static void main(String[] args) 
+    {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+    public static String xorEncode(String data)
+    {
+        try
+        {
+            String xor = "";
+
+            for(int i = 0; i < data.length(); i++)
+                xor += (char)(data.charAt(i) ^ XOR_KEY.charAt(i % XOR_KEY.length()));
+
+            return xor;
+        }
+        catch(Exception err)
+        {
+            err.printStackTrace();
+            return null;
+        }
+    }
+}
